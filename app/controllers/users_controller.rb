1886047c346cd1ed0d9e26059ea87b0faa738731
Neil Spurgeon <neilspurgeon@gmail.com>
2015-06-15 14:00:28 -0700

    create user, edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,17 +9,33 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.create(user_params)
+    redirect_to user_path(@user)
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    set_user
+    @user = User.find(params[:id])
   end
 
   def update
+    # @use
   end
 
   def destroy
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :pasword_confirmation)
+  end
+
+  def set_user
+    @user = current_user
+  end
+
 end
