d0a99e8b3424727ca1df0d829cf81b9705ac7d4a
Neil Spurgeon <neilspurgeon@gmail.com>
2015-06-16 15:08:16 -0700

    checks signed_in? on stories

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,7 @@
 class StoriesController < ApplicationController
   before_action :set_story, only: [:show, :edit, :update, :destroy]
   before_action :set_user
+  before_action :signed_in?, except: [:index, :show]
   # GET /stories
   # GET /stories.json
   def index
