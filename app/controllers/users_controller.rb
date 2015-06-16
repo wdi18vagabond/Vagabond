0cc54ff676151bce215ab6c3d305cbcedc1c0724
Neil Spurgeon <neilspurgeon@gmail.com>
2015-06-16 13:40:55 -0700

    added username and city

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :pasword_confirmation)
+    params.require(:user).permit(:first_name, :last_name, :username, :city, :email, :password, :pasword_confirmation)
   end
 
   def set_user
