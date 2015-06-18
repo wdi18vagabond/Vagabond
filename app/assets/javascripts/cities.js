b54bb0b27d25862d2599a1698ac47f4e59ac79ca
cmswalker <c.michael.s.walker@gmail.com>
2015-06-17 17:53:00 -0700

    started parsing map search queries

--- app/assets/javascripts/cities.js
+++ app/assets/javascripts/cities.js
@@ -3,11 +3,16 @@ var ready;
 var address = $('#city_name h2').text();
 var vagabond_map = {};
 var map;
+var markers = [];
+var places;
 var geocoder;
 
 vagabond_map.map = map;
+vagabond_map.puppy = "http://www.lindyop.com/ESW/Images/puppy.png";
+vagabond_map.markers = markers;
 vagabond_map.geocoder = geocoder;
 vagabond_map.address = address;
+vagabond_map.places = places;
 vagabond_map.styles = [
             {
               stylers: [
@@ -39,17 +44,14 @@ vagabond_map.ready = function() {
 
 vagabond_map.initialize = function() {
 	this.geocoder = new google.maps.Geocoder();
-
   this.map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 12,
     minZoom: 10,
     maxZoom: 16,
     zoomControl: false
   });
-
     this.map.setOptions({styles: this.styles});
     this.codeAddress();
-    //map_search();
 };//END INITIALIZE
 
 vagabond_map.codeAddress = function() {
@@ -60,14 +62,9 @@ vagabond_map.codeAddress = function() {
   var self = this;
   this.geocoder.geocode( { 'address': address}, function(results, status) {
       console.log(results);
-      console.log(results[0].geometry.location);
       console.log(results[0].geometry.location["A"])
       console.log(results[0].geometry.location["F"])
-
-
       self.map.setCenter(results[0].geometry.location);
-     
-
       var marker = new google.maps.Marker({
           map: vagabond_map.map,
           position: results[0].geometry.location
@@ -80,23 +77,23 @@ vagabond_map.codeAddress = function() {
 vagabond_map.map_search = function() {
 
   var self = this;
-  var markers = [];
   this.map.controls[google.maps.ControlPosition.TOP_RIGHT].push(this.input);
   // Listen for the event fired when the user selects an item from the
   // pick list. Retrieve the matching places for that item.
   google.maps.event.addListener(self.search_box, 'places_changed', function() {
-    var places = self.search_box.getPlaces();
+    self.places = self.search_box.getPlaces();
 
-    if (places.length == 0) {
+    if (self.places.length == 0) {
       return;
     }
-    for (var i = 0, marker; marker = markers[i]; i++) {
+
+    for (var i = 0, marker; marker = self.markers[i]; i++) {
       marker.setMap(null);
     }
     // For each place, get the icon, place name, and location.
-    markers = [];
+    self.markers = [];
     var bounds = new google.maps.LatLngBounds();
-    for (var i = 0, place; place = places[i]; i++) {
+    for (var i = 0, place; place = self.places[i]; i++) {
       var image = {
         url: place.icon,
         size: new google.maps.Size(71, 71),
@@ -112,17 +109,61 @@ vagabond_map.map_search = function() {
         position: place.geometry.location
       });
       //push these into the marker array
-      markers.push(marker);
+      self.markers.push(marker);
       bounds.extend(place.geometry.location);
     }
     self.map.fitBounds(bounds);
+    self.gather_places();
   }); // END SEARCHBOX ON CHANGE LISTENER
+
+
   // Bias the SearchBox results towards places that are within the bounds of the
   // current map's viewport.
   google.maps.event.addListener(vagabond_map.map, 'bounds_changed', function() {
     var bounds = vagabond_map.map.getBounds();
     self.search_box.setBounds(bounds);
   });
-}
+}// END map_search
+
+vagabond_map.gather_places = function () {
+  this.current_place_ids = [];
+  this.current_markers = [];
+  this.places.forEach(function (v, i) {
+    for(key in v) {
+      if (key === "name") {
+        vagabond_map.current_place_ids.push({name : v[key]});
+        //console.log(i);
+      }
+      if (key === "place_id") {
+        vagabond_map.current_place_ids[i].place_id = v[key];
+      }
+    }
+  });
+  
+  for (var i = 0, place; place = vagabond_map.places[i]; i++) {
+    var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+    // Create a marker for each place.
+    var marker = new google.maps.Marker({
+      map: vagabond_map.map,
+      icon: image,
+      title: place.name,
+      position: place.geometry.location
+    });
+    //push these into the marker array
+    self.markers.push(marker);
+    vagabond_map.current_markers.push({latitude : self.markers[i]["position"]["A"]});
+    vagabond_map.current_markers[i].longitude = self.markers[i]["position"]["F"];
+    //Optional for re rendering
+    //bounds.extend(place.geometry.location);
+  }
+  console.log(this.current_place_ids, this.current_place_ids.length);
+  console.log(this.current_markers, this.current_markers.length);
+};
 
 
